87b6f392f4f62d5e986ac694a3663902fc870b4b
karthigs <karthigs108@gmail.com>
2021-12-08 23:47:14 +0530

    first commit

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,11 @@
+const MyToken = artifacts.require("MyToken");
+const Crowdsale = artifacts.require("Crowdsale");
+
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(MyToken);
+  const token = await MyToken.deployed();
+
+  await deployer.deploy(Crowdsale, 10, accounts[0], token.address);
+  const crowdsale = await Crowdsale.deployed();
+  token.transfer(crowdsale.address, await token.totalSupply())
+};
